935a80c3a53831e27dfd2f42e3523a0bf155a908
AndrewMTitus <112717415+AndrewMTitus@users.noreply.github.com>
2024-08-18 10:27:43 -0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,11 +2,38 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi import HTTPException, status
 from passlib.context import CryptContext
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-# Change these values in a real application
+
 SECRET_KEY = "4163979aafcfbef09c4feeba2a9e826ec0dc7ac52f4d819c54c3a2a478957dcb"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(self, request):
+        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
+        if credentials:
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail="Invalid authentication credentials"
+                )
+            return credentials.credentials
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid authorization code."
+            )
+
+    def verify_jwt(self, jwtoken: str) -> bool:
+        isTokenValid: bool = False
+        try:
+            payload = jwt.decode(jwtoken, SECRET_KEY, algorithms=[ALGORITHM])
+            isTokenValid = True
+        except:
+            payload = None
+        return isTokenValid
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
